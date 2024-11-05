b67cbafdb43c76106e5f36af0d35c32686d0831e
Juliano Ventola <ventolajuliano@gmail.com>
2024-11-05 14:55:52 -0300

    feat: adds sounds and font

--- src/main.ts
+++ src/main.ts
@@ -25,3 +25,12 @@ k.loadSprite("motobug", "graphics/motobug.png", {
     spin: { from: 0, to: 4, loop: true, speed: 8 }
   }
 })
+
+k.loadFont("mania", "fonts/mania.ttf")
+
+k.loadSound("city", "sounds/city.mp3")
+k.loadSound("destroy", "sounds/Destroy.wav")
+k.loadSound("hurt", "sounds/Hurt.wav")
+k.loadSound("hyper-ring", "sounds/HyperRing.wav")
+k.loadSound("jump", "sounds/Jump.wav")
+k.loadSound("ring", "sounds/Ring.wav")
